77353a34ea854262c91b0babf16eabb9c3c2ac87
alexrivas05 <alex12rivas34@gmail.com>
2026-03-25 11:58:03 -0400

    Designed quiz layout. Have it pick a word, show definitons, and show a submit.

--- routes/quiz.js
+++ routes/quiz.js
@@ -2,12 +2,39 @@ const express = require('express');
 const router = express.Router();
 const {readFile} = require('fs').promises;
 
-router.get("/", (req, res)=>{
+router.get("/", async (req, res)=>{
+    let chosenWords = await getWords();
+    console.log("Chosen: ", chosenWords);
+    res.render('quiz', {chosenWords});
+
+});
+router.post("/", (req, res) =>{
+    console.log(req.body);
 
 });
 
 let getWords = async ()=>{
+    //console.log("Getting Random Part!");
     let randomPart = getRandomPart();
+    let allWords = await readFile('resource/allwords.txt', 'utf8');
+    let wordArray = allWords.split('\n');
+    //console.log(wordArray);
+    shuffle(wordArray);
+
+    let choices = [];
+    while(choices.length < 5) {
+        let line = wordArray.pop();
+        let tokens = line.split('\t');
+        let word = tokens[0];
+        let part = tokens[1];
+        let def = tokens[2];
+
+        if(part == randomPart){
+            choices.push(line);
+        }
+
+    }
+    return choices;
 }
 let getRandomPart = ()=>{
     let parts = ['noun', 'verb', 'adjective'];
@@ -17,7 +44,7 @@ let getRandomPart = ()=>{
 }
 
 let shuffle = (array)=>{
-    for(let i = 0;i<array.length-1;i--){
+    for(let i = array.length-1;i >0;i--){
         let randomNumber = Math.floor(Math.random()*(i+1));
         [array[i], array[randomNumber]] = [array[randomNumber], array[i]];
     }
